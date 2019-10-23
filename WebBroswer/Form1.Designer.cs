8d7d149221f3aaecb7ce4fbaec8eea41bd23f429
ali-shahKust <syed.ali.shah3938@gmail.com>
2019-10-23 10:05:02 +0500

    First Commit

---
+++ WebBroswer/Form1.Designer.cs
@@ -0,0 +1,170 @@
+﻿namespace WebBroswer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.tableLayoutPanel2 = new System.Windows.Forms.TableLayoutPanel();
+            this.backBtn = new System.Windows.Forms.Button();
+            this.ForwardBtn = new System.Windows.Forms.Button();
+            this.refreshBtn = new System.Windows.Forms.Button();
+            this.NextBtn = new System.Windows.Forms.Button();
+            this.myUrl = new System.Windows.Forms.TextBox();
+            this.myWebView = new System.Windows.Forms.WebBrowser();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.tableLayoutPanel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 1;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.Controls.Add(this.tableLayoutPanel2, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.myWebView, 0, 1);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 2;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 5.572755F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 94.42725F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(765, 646);
+            this.tableLayoutPanel1.TabIndex = 0;
+            // 
+            // tableLayoutPanel2
+            // 
+            this.tableLayoutPanel2.ColumnCount = 5;
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 45F));
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 45F));
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 65F));
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 50F));
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 526F));
+            this.tableLayoutPanel2.Controls.Add(this.NextBtn, 3, 0);
+            this.tableLayoutPanel2.Controls.Add(this.refreshBtn, 2, 0);
+            this.tableLayoutPanel2.Controls.Add(this.ForwardBtn, 1, 0);
+            this.tableLayoutPanel2.Controls.Add(this.backBtn, 0, 0);
+            this.tableLayoutPanel2.Controls.Add(this.myUrl, 4, 0);
+            this.tableLayoutPanel2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel2.Location = new System.Drawing.Point(3, 3);
+            this.tableLayoutPanel2.Name = "tableLayoutPanel2";
+            this.tableLayoutPanel2.RowCount = 1;
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel2.Size = new System.Drawing.Size(759, 29);
+            this.tableLayoutPanel2.TabIndex = 0;
+            // 
+            // backBtn
+            // 
+            this.backBtn.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.backBtn.Location = new System.Drawing.Point(3, 3);
+            this.backBtn.Name = "backBtn";
+            this.backBtn.Size = new System.Drawing.Size(39, 23);
+            this.backBtn.TabIndex = 0;
+            this.backBtn.Text = "<<";
+            this.backBtn.UseVisualStyleBackColor = true;
+            this.backBtn.Click += new System.EventHandler(this.backBtn_Click);
+            // 
+            // ForwardBtn
+            // 
+            this.ForwardBtn.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.ForwardBtn.Location = new System.Drawing.Point(48, 3);
+            this.ForwardBtn.Name = "ForwardBtn";
+            this.ForwardBtn.Size = new System.Drawing.Size(39, 23);
+            this.ForwardBtn.TabIndex = 1;
+            this.ForwardBtn.Text = ">>";
+            this.ForwardBtn.UseVisualStyleBackColor = true;
+            this.ForwardBtn.Click += new System.EventHandler(this.ForwardBtn_Click);
+            // 
+            // refreshBtn
+            // 
+            this.refreshBtn.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.refreshBtn.Location = new System.Drawing.Point(93, 3);
+            this.refreshBtn.Name = "refreshBtn";
+            this.refreshBtn.Size = new System.Drawing.Size(59, 23);
+            this.refreshBtn.TabIndex = 2;
+            this.refreshBtn.Text = "Refresh";
+            this.refreshBtn.UseVisualStyleBackColor = true;
+            this.refreshBtn.Click += new System.EventHandler(this.refreshBtn_Click);
+            // 
+            // NextBtn
+            // 
+            this.NextBtn.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.NextBtn.Location = new System.Drawing.Point(158, 3);
+            this.NextBtn.Name = "NextBtn";
+            this.NextBtn.Size = new System.Drawing.Size(44, 23);
+            this.NextBtn.TabIndex = 3;
+            this.NextBtn.Text = "Go";
+            this.NextBtn.UseVisualStyleBackColor = true;
+            this.NextBtn.Click += new System.EventHandler(this.NextBtn_Click);
+            // 
+            // myUrl
+            // 
+            this.myUrl.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.myUrl.Location = new System.Drawing.Point(208, 3);
+            this.myUrl.Name = "myUrl";
+            this.myUrl.Size = new System.Drawing.Size(548, 20);
+            this.myUrl.TabIndex = 4;
+            this.myUrl.TextChanged += new System.EventHandler(this.myUrl_TextChanged);
+            // 
+            // myWebView
+            // 
+            this.myWebView.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.myWebView.Location = new System.Drawing.Point(3, 38);
+            this.myWebView.MinimumSize = new System.Drawing.Size(20, 20);
+            this.myWebView.Name = "myWebView";
+            this.myWebView.Size = new System.Drawing.Size(759, 605);
+            this.myWebView.TabIndex = 1;
+            this.myWebView.DocumentCompleted += new System.Windows.Forms.WebBrowserDocumentCompletedEventHandler(this.myWebView_DocumentCompleted);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(765, 646);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel2.ResumeLayout(false);
+            this.tableLayoutPanel2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel2;
+        private System.Windows.Forms.Button NextBtn;
+        private System.Windows.Forms.Button refreshBtn;
+        private System.Windows.Forms.Button ForwardBtn;
+        private System.Windows.Forms.Button backBtn;
+        private System.Windows.Forms.TextBox myUrl;
+        private System.Windows.Forms.WebBrowser myWebView;
+    }
+}
+
---
+++ WebBroswer/Form1.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WebBroswer
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void tableLayoutPanel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void myWebView_DocumentCompleted(object sender, WebBrowserDocumentCompletedEventArgs e)
+        {
+        }
+
+        private void NextBtn_Click(object sender, EventArgs e)
+        {
+
+            myWebView.Navigate(myUrl.Text);
+
+        }
+
+        private void myUrl_TextChanged(object sender, EventArgs e)
+        {
+            myWebView.Navigate(myUrl.Text);
+
+        }
+
+        private void ForwardBtn_Click(object sender, EventArgs e)
+        {
+            myWebView.GoForward();
+        }
+
+        private void backBtn_Click(object sender, EventArgs e)
+        {
+            myWebView.GoBack();
+        }
+
+        private void refreshBtn_Click(object sender, EventArgs e)
+        {
+            myWebView.Refresh();
+        }
+    }
+}
